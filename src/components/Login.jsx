c6274e14b87c2feaafc86695421fb48528fe8fa3
Ramprasadya <yadavramprasad563@gmail.com>
2024-04-27 13:49:52 +0530

    login end point

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 
 const Login = () => {
+  const handleSubmit=async(e)=>{
+    e.preventDefault()
+    const response = await fetch(`http://localhost:3000/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const jsn =  await response.json()
+    console.log(jsn)
+  }
   return (
    <>
    <div className="container flex mx-auto justify-center mt-4">
 
-    <form>
+    <form onSubmit={handleSubmit} >
   <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">Email address</label>
+    <label for="exampleInputEmail1" name="email" className="form-label">Email address</label>
     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
     {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
   </div>
   <div className="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"/>
+    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" name="password" id="exampleInputPassword1"/>
   </div>
   <button  type="submit" className="py-2 px-3 bg-violet-600 text-white rounded-[10px] ">Submit</button>
 </form>
