9c1aee71ced380f9a42fdc6947c726733c6a26e3
Ramprasadya <yadavramprasad563@gmail.com>
2024-04-29 14:56:30 +0530

    signup page complete

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
 
     <form onSubmit={handleSubmit} >
   <div className="mb-3">
-    <label for="exampleInputEmail1" name="email" className="form-label">Email address</label>
+    <label htmlFor="exampleInputEmail1" name="email" className="form-label">Email address</label>
     <input type="email" className="form-control" onChange={(e)=>setEmail(e.target.value)} id="exampleInputEmail1" aria-describedby="emailHelp"/>
     {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
   </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Signup
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3000/api/auth/createuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+    const json = await response.json();
+    console.log(json);
+    navigate("/")
+  };
+  return (
+    <div>
+      <div className="container flex mx-auto justify-center mt-4">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label
+              htmlFor="exampleInputEmail1"
+              name="name"
+              className="form-label"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              onChange={(e) => setName(e.target.value)}
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label
+              htmlFor="exampleInputEmail1"
+              name="email"
+              className="form-label"
+            >
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              onChange={(e) => setEmail(e.target.value)}
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button
+            type="submit"
+            className="py-2 px-3 bg-violet-600 text-white rounded-[10px] "
+          >
+            Signup
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
