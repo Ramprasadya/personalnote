f771abd88e5e6c32fca11cb28b3aed8e29aaca86
Ramprasadya <yadavramprasad563@gmail.com>
2024-04-29 14:11:07 +0530

    login done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const handleSubmit=async(e)=>{
     e.preventDefault()
     const response = await fetch(`http://localhost:3000/api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
-      }
+      },
+      body : JSON.stringify({email,password})
     });
     const jsn =  await response.json()
-    console.log(jsn)
+    if(jsn.success){
+      // alert('login')
+      console.log(jsn)
+
+    }else{
+      alert("Inavalid ")
+    }
   }
   return (
    <>
@@ -19,12 +28,12 @@ const Login = () => {
     <form onSubmit={handleSubmit} >
   <div className="mb-3">
     <label for="exampleInputEmail1" name="email" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <input type="email" className="form-control" onChange={(e)=>setEmail(e.target.value)} id="exampleInputEmail1" aria-describedby="emailHelp"/>
     {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
   </div>
   <div className="mb-3">
     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" name="password" id="exampleInputPassword1"/>
+    <input type="password" className="form-control" onChange={(e)=>setPassword(e.target.value)} name="password" id="exampleInputPassword1"/>
   </div>
   <button  type="submit" className="py-2 px-3 bg-violet-600 text-white rounded-[10px] ">Submit</button>
 </form>
